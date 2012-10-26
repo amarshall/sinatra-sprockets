b7ca29dc9b01013cb65d193532ea97ea28eb997c
Andrew Marshall <andrew@johnandrewmarshall.com>
2012-10-25 21:14:26 -0400

    Simplify configuration class
    - Move DEFAULT_OPTIONS into Configuration
    - Don't use struct when we end up reimplementing everything it gives us
      anyway
    - Use instance_variable_set over send(:foo=)
    - Fix Ruby warning

--- lib/sinatra/sprockets/configuration.rb
+++ lib/sinatra/sprockets/configuration.rb
@@ -1,26 +1,25 @@
 module Sinatra::Sprockets
-  DEFAULT_OPTIONS = {
-    app: nil,
-    debug: false,
-    digest: false,
-    compile: false,
-    compress: false,
-    host: nil,
-    manifest_path: 'public/assets',
-    paths: %w[images javascripts stylesheets],
-    precompile: [ /\w+\.(?!js|css).+/, /application.(css|js)$/ ],
-    prefix: 'assets',
-    relative_url_root: ENV['RACK_RELATIVE_URL_ROOT'],
-    css_compressor: false,
-    js_compressor: false
-  }
-
-  class Configuration < Struct.new(*DEFAULT_OPTIONS.keys)
-    attr_accessor *DEFAULT_OPTIONS.keys, :digests
+  class Configuration
+    DEFAULT_OPTIONS = {
+      app: nil,
+      debug: false,
+      digest: false,
+      compile: false,
+      compress: false,
+      host: nil,
+      manifest_path: 'public/assets',
+      paths: %w[images javascripts stylesheets],
+      precompile: [ /\w+\.(?!js|css).+/, /application.(css|js)$/ ],
+      prefix: 'assets',
+      relative_url_root: ENV['RACK_RELATIVE_URL_ROOT'],
+      css_compressor: false,
+      js_compressor: false
+    }
+    attr_accessor :digests, *DEFAULT_OPTIONS.keys
 
     def initialize
       DEFAULT_OPTIONS.each do |key, value|
-        send "#{key}=".to_sym, value
+        instance_variable_set :"@#{key}", value
       end
       @digests = {}
     end
